0596a1ff3bdf4105c412c42014a8f14dba79fc1c
Ikramul-m <i20727172@gmail.com>
2024-05-01 14:54:28 +0530

    Express

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>HOME</h1>");
+  console.log(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.post("/", (req, res) => {
