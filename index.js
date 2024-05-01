6a83d8fb6da761107c71f044dde2172199968911
Ikramul-m <i20727172@gmail.com>
2024-05-01 15:10:09 +0530

    Express

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 4000;
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   console.log(path.join(__dirname + "/index.html"));
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.post("/", (req, res) => {
-  const userName = req.body.name;
+app.post("/api/v1/login", (req, res) => {
+  res.send("<h1>Done</h1>");
+  console.log(req.body);
 });
 
 app.listen(port, () => {
