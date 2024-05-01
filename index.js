5b431658c9a651431184728e14495a0ece289994
Ikramul-m <i20727172@gmail.com>
2024-05-01 19:43:26 +0530

    Express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 4000;
+const hostName = "localhost";
+
+app.listen(PORT, hostName, () => {
+  console.log(`Server is working on http://${hostName}:${PORT}`);
+});
