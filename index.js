1273f9fa8dee5579ea51aa6f56e8647bbce62bea
Ikramul-m <i20727172@gmail.com>
2024-05-01 19:44:39 +0530

    Express

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const PORT = 4000;
 const hostName = "localhost";
 
+app.arguments("/");
+
 app.listen(PORT, hostName, () => {
   console.log(`Server is working on http://${hostName}:${PORT}`);
 });
