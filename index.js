fe6dce86c8b75152a92c6ffc0f0fef120fffe29f
Ikramul-m <i20727172@gmail.com>
2024-05-01 19:39:59 +0530

    Express

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
+port = 4000;
+hostName = "localhost";
+
+app.listen(port, hostName, () => {
+  console.log(`Server is working on http://${hostName}:${port}`);
+});
