fc0f88028e0e6135fb73bd270031be572b34b79d
Ikramul-m <i20727172@gmail.com>
2024-05-01 14:09:10 +0530

    Express

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>HELLO WORLD</h1>");
+  res.send("<h1>HOME</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>ABOUT </h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>CONTACT</h1>");
 });
 
 app.listen(port, () => {
