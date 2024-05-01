6a2d4f610b550bdb765eabeabe460acc8ea37c3e
Ikramul-m <i20727172@gmail.com>
2024-05-01 13:48:38 +0530

    Express

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.get("/", (req, res) => {
+  res.send("<h1>HELLO WORLD</h1>");
+});
+
 app.listen(port, () => {
   console.log(`Server is working on port: ${port}`);
 });
