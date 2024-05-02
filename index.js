91e41f3f298b71d37a954009ea382d2842faa354
Ikramul-m <i20727172@gmail.com>
2024-05-02 09:02:15 +0530

    Express

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ const PORT = 4000;
 const hostName = "localhost";
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+app.get("/api/v1/userData", (req, res) => {
+  res.json({
+    name: "Ikram",
+    email: "kram@gamil.com",
+    password: "password"
+  })
 });
 
 app.post("/api/v1/login", (req, res) => {
   // const usename = req.body.name;
-  res.send(`Name: ${req.body.name}\nEmail: ${req.body.email}}`);
+  res.send(`<h2>Name: ${req.body.name}</h2> <h2>Email: ${req.body.email}</h2>`);
   // console.log("name");
   console.log(req.body);
 });
