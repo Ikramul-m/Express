a4eede01c1f117791106f54c51b04d09aa2dd527
Ikramul-m <i20727172@gmail.com>
2024-05-02 10:10:16 +0530

    Express

--- UserRouter.js
+++ UserRouter.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const registerUser = require("./UserController");
 
-router.route("/api/v1/register").post(registerUser);
+router.route("/api/v1/register").post(registerUser).get((req,res) => {
+    res.send("working")
+})
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,21 +3,18 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const router = require("./UserRouter");
 
-// const { emit } = require("process");
-
 const app = express();
 
 const PORT = 4000;
 const hostName = "localhost";
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(router);
+app.use("/api/v1", router);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
-
-// app.post("/",);
 
 app.listen(PORT, hostName, () => {
   console.log(`Server is working on http://${hostName}:${PORT}`);
